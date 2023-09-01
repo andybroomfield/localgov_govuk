3638824de3cc60e412b5aa441d0c9a6eb682a87e
Andy Broomfield <andybroomfield@gmail.com>
2023-09-01 21:03:54 +0100

    Initial commit
    Basic child theme of GovUK theme for Localgov Drupal
    Currently just the child theme with some modifications to acccount for
    emergency banners (localgov alert banners)
    
    - Creates the child theme
    - Adds custom page template with renderable header region below the header
    - Adds localgov alert banner template with classes for the emergency banner pattern
    - Adds styles for rendering the alert banners using the emergency banner pattern
    - Removes the blue bar from the navigation, renders it as a seperate compnent like on gov.uk
    - Use a generic class for targeting styles at the menu bar block

---
+++ js/localgov-govuk.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Localgov Gov.UK behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.localgovGovuk = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
